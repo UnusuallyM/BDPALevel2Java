5bbb0824665a80bc19efc41b05b414b5d0fd5ebc
unknown <BDPA22@metrostate.edu>
2018-02-24 14:05:07 -0600

    Saving my work

--- super-hero-page/src/main/java/com/example/superheropage/SuperHeroController.java
+++ super-hero-page/src/main/java/com/example/superheropage/SuperHeroController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SuperHeroController {
     @RequestMapping("/superhero")
     public String greeting(Model model) {
-        model.addAttribute("name", "Super Hero Name");
-        model.addAttribute("power1", "A power that is super");
-        model.addAttribute("power2", "Another power that is super");
+        model.addAttribute("name", "Maniac Creator");
+        model.addAttribute("power1", "Other wordly mathmatically solving brain");
+        model.addAttribute("power2", "Scientifically ");
         model.addAttribute("power3", "A third power that is super");
         model.addAttribute("fact1", "This is fun fact is maybe not so fun");
         model.addAttribute("fact2", "This is another fun fact");
